e89579f1e92abe71404f4766eed51a1198d115dc
satendra-sr <officialsatendrarawat@gmail.com>
2020-07-17 11:18:59 +0530

    Added structure for pages and coming soon template :construction_worker:

--- wp-content/themes/tides/main.js
+++ wp-content/themes/tides/main.js
@@ -1,3 +1,3 @@
-/*! Tides 2020-07-15 */
+/*! Tides 2020-07-17 */
 
 jQuery(document).ready(function(){jQuery(".navbar-toggle").on("click",function(){jQuery(this).hasClass("navbar-toggle-cross")?(jQuery(".mobile-menu").removeClass("d-flex"),jQuery("html, body").removeClass("overflow-hidden position-relative h-100p")):(jQuery(".mobile-menu").addClass("d-flex"),jQuery("html, body").addClass("overflow-hidden position-relative h-100p")),jQuery(this).toggleClass("navbar-toggle-cross")})});
---
+++ wp-content/themes/tides/templates/template-coming-soon.php
@@ -0,0 +1,19 @@
+  
+<?php
+/**
+ * Template Name: Coming soon
+ *
+ * @package tides
+ */
+get_header();
+?>
+
+<div class="d-flex justify-content-center align-items-center h-screen-100">
+	<div>
+		<h1 class="text-center display-3 mb-5">Coming soon!</h1>
+		<h3 class="text-center">Please check back later.</h3>
+	</div>
+</div>
+
+<?php
+get_footer();
